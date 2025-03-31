8a82cbb11237a85f465d28dab7a5574cfca7e2bc
Nicepawer7 <nicepeperone07@gmail.com>
2025-03-31 13:27:23 +0200

    finito test accelerazione PID

--- Race/development/andamento_PID.py
+++ Race/development/andamento_PID.py
@@ -1,23 +1,37 @@
 import matplotlib.pyplot as plt
 from math import sqrt as radice
 velocitàMax = 100
-distanza = 1000
-kCurva = 3
+distanza = 3000
+kCurva = distanza/4
 percorsa = []
+velocità = []
 i = 0
 
-velocità = []
-while i < distanza/kCurva/2:
-    velocità.append(radice(abs(((i-(distanza/kCurva/2))**2)/((distanza/kCurva/2)**2)-1)*(velocitàMax-30)**2)+30) 
-    print(radice(abs(((i-(distanza/kCurva/2))**2)/((distanza/kCurva/2)**2)-1)*(velocitàMax-30)**2)+30) #prima ellisse sezionata
-    percorsa.append(i)
-    i += 1
-
 fig, ax = plt.subplots()
-ax.plot(percorsa,velocità)
-ax.plot([0,0],[30,100],color="green",linestyle="dashed")
-ax.plot([0,distanza],[30,30],color="green",linestyle="dashed")
 ax.set(xlabel='Gradi obbiettivo', ylabel='Velocità',
        title='Funzione della velocità in relazione alla rotazione mancante')
 ax.grid()
+ax.plot([0,0],[30,100],color="black",linestyle="dashed")
+ax.plot([distanza,distanza],[30,velocitàMax],color="black",linestyle="dashed")
+ax.plot([0,distanza],[30,30],color="black",linestyle="dashed")
+ax.plot([0,distanza],[100,100],color="black",linestyle="dashed")
+
+while i < kCurva:
+    #velocità.append(radice(abs(((i-(distanza/kCurva/2))**2)/((distanza/kCurva/2)**2)-1)*(velocitàMax-30)**2)+30) 
+    velocità.append(radice(((((i-kCurva)**2)/kCurva**2)-1)*(-(velocitàMax-30)**2))+30)
+    print(radice(((((i-kCurva)**2)/kCurva**2)-1)*(-(velocitàMax-30)**2))+30) #prima ellisse sezionata
+    percorsa.append(i)
+    i += 1
+
+while kCurva <= i <= distanza-kCurva:
+    percorsa.append(i)
+    velocità.append(velocitàMax)
+    i += 1
+while distanza-kCurva<= i <= distanza:
+    velocità.append(radice(((((i-distanza+kCurva)**2)/kCurva**2)-1)*(-(velocitàMax-30)**2))+30)
+    print(radice(((((i-distanza+kCurva)**2)/kCurva**2)-1)*(-(velocitàMax-30)**2))+30)
+    percorsa.append(i)
+    i += 1
+
+ax.plot(percorsa,velocità,color="red",linewidth=2)
 plt.show()
