d9f1f9627f45cc1aed5e30458872b80932dbc13c
Nicepawer7 <157480620+Nicepawer7@users.noreply.github.com>
2025-04-05 23:37:01 +0200

    finito e testato movement_logger funzionante

--- Race/utilities/movement_logger.py
+++ Race/utilities/movement_logger.py
@@ -8,34 +8,41 @@ C = Motor('C')
 D = Motor('D')
 programmaSelezionato = ["ARROW_N","GHOST","GO_LEFT","GO_RIGHT"]
 def move_log(): # aggiungere calcolo per indietro con bottone destro
+    l = 0
+    r = 0
     lStart = motoreSinistro.get_degrees_counted() #posizione iniziale sinistra
     rStart = motoreDestro.get_degrees_counted()
     while spike.left_button.is_pressed() == False: # attende di essere premuto e nel mentre conta i gradi
         lPercorsi = motoreSinistro.get_degrees_counted() #spazio percorso
         rPercorsi = motoreDestro.get_degrees_counted()
-    l = abs(lPercorsi) - lStart # potrebbe non funzionare andando indietro
-    r = abs(rPercorsi) - rStart
-    avg = str((l + r)/2) # media dello spazio percorso
-    print("Gradi percorsi dritto: " + avg + " gradi")
+    l = (lPercorsi - lStart)*-1 # potrebbe non funzionare andando indietro
+    r = rPercorsi - rStart
+
+    avg = int((l + r)/2) # media dello spazio percorso
+    print("Distanza media " + str(avg) + " gradi")
     return
 
 def turn_log():
     startGyro = spike.motion_sensor.get_yaw_angle()
     while spike.left_button.is_pressed() == False: # attende di essere premuto e nel mentre conta i gradi di rotazione
         gyro = spike.motion_sensor.get_yaw_angle()
-    print("Gradi girati: " + str(startGyro-gyro) + " verso un lato") # potrebbe essere necessario normalizzare i gradi in qualche modo
+    ruotati = startGyro-gyro
+    if ruotati > 0:
+        print("Girato di " + str(ruotati) + " gradi verso sinistra") # potrebbe essere necessario normalizzare i gradi in qualche modo
+    if ruotati < 0:
+        print("Girato di " + str(abs(ruotati)) + " gradi verso destra") # potrebbe essere necessario normalizzare i gradi in qualche modo
 
 def left_log():
     lStart = C.get_degrees_counted() # gradi iniziali motore sinistro
     while spike.left_button.is_pressed() == False:
         l = C.get_degrees_counted() # gradi finchè I/O sinistro non viene premuto
-    print("Motore sinistro ruotato di: " + str(l-lStart) + " gradi")
+    print("Motore sinistro ruotato di " + str(l-lStart) + " gradi")
 
 def right_log():
     rStart = D.get_degrees_counted()
     while spike.left_button.is_pressed() == False:
         r = D.get_degrees_counted()
-    print("Motore destro ruotato di: " + str(r-rStart) + " gradi")
+    print("Motore destro ruotato di " + str(r-rStart) + " gradi")
 
 def start(i):
     if i == 0:
@@ -55,14 +62,14 @@ def start(i):
 
 def manager():
     i = 0
-    spike.light_matrix.show_image("ANGRY")
+    spike.light_matrix.show_image(programmaSelezionato[i])
     spike.status_light.on('red')
     while True:
         if spike.left_button.is_pressed():
             time.sleep(0.50)
             spike.status_light.on('green')
             i = start(i)
-            spike.light_matrix.show_image("ANGRY")
+            spike.light_matrix.show_image(programmaSelezionato[i])
             spike.status_light.on('red')
         if spike.right_button.is_pressed():
             time.sleep(0.50)
@@ -74,6 +81,9 @@ def manager():
             if i == 2:
                 spike.light_matrix.show_image(programmaSelezionato[i])
             if i == 3:
+                spike.light_matrix.show_image(programmaSelezionato[i])
+            if i == 4:
+                i = 0
                 spike.light_matrix.show_image(programmaSelezionato[i])
             
 
